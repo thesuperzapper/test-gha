2db50ae9926c68cc9005bca6a9b18067509df18c
Mathew Wicks <thesuperzapper@users.noreply.github.com>
2023-07-19 23:47:57 -0700

    action which parses multiple outputs

--- scripts/test_1.sh
+++ scripts/test_1.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 cd "$THIS_SCRIPT_PATH"
 
-OUTPUT_FOLDER="../output/test_1/"
+export OUTPUT_FOLDER="../output/test_1/"
 
 # make and populate the output folder
 mkdir -p "$OUTPUT_FOLDER"
